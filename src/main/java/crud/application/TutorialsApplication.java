b0c01236e299333d0104d1e3c6be7204a0d9cf99
Iskren Petrov <54028062+iskrenpetrov@users.noreply.github.com>
2020-06-27 19:11:35 +0300

    Update TutorialsApplication.java

--- src/main/java/crud/application/TutorialsApplication.java
+++ src/main/java/crud/application/TutorialsApplication.java
@@ -3,7 +3,7 @@ package crud.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //enables Spring Boot autoconfiguration and component scanning
 public class TutorialsApplication {
 
 	public static void main(String[] args) {
