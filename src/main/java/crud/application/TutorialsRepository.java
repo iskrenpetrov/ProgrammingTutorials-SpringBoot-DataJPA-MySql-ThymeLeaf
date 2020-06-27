0d421cdfc392c1c92613a3eb9b595f7c15a1bcfb
Iskren Petrov <54028062+iskrenpetrov@users.noreply.github.com>
2020-06-27 19:12:31 +0300

    Update TutorialsRepository.java

--- src/main/java/crud/application/TutorialsRepository.java
+++ src/main/java/crud/application/TutorialsRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface TutorialsRepository extends JpaRepository<Tutorials, Long> {
+    //@Query annotation  — its value attribute contains the JPQL or SQL to execute.
     @Query("SELECT tut FROM Tutorials tut WHERE CONCAT(tut.name,tut.content,tut.author) LIKE %?1%")
     public List<Tutorials> search(String keyword);
 }
